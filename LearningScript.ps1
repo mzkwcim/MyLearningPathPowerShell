8296d52aebeac7f478e0d0dddcf7b49792472fa3
Waldemar Krakowiak <wkrak98@gmail.com>
2024-01-30 21:44:57 +0100

    first comment

---
+++ LearningScript.ps1
@@ -0,0 +1,27 @@
+﻿Set-StrictMode -Version Latest
+
+function Create-Configuration{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$Name,
+        [Parameter()]
+        [ValidateSet("1","2")]
+        [string] $Version="1"
+    )
+
+    begin{
+        Write-Host "Creating configuration file with the name"
+    }
+
+    process{
+        Write-Host "Creating configuration file with the name $Name and version $Version"
+    }
+
+    end{
+        Write-Host "Creating configuration file with the name"
+    }
+}
+
+$Names = @("Test1", "Test2", "Test3")
+$Names | Create-Configuration -Version 2
